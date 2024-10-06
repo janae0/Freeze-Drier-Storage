4c2edfcf4c2c918b1a9b4a1ca8ed3ec6274b4424
LilyS0 <wds6609@rit.edu>
2024-10-05 22:29:50 -0400

    linting

--- freeze-drier-app/app/index.tsx
+++ freeze-drier-app/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Here is some sample text</Text>
     </View>
   );
 }
